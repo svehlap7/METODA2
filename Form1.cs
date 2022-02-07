8287d133eef468fcf0ea21de283439ff0f53181a
Patrik Švehla <svehlap.03@spst.eu>
2022-02-07 12:52:50 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace METODA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool Prvocislo(int x)
+        {
+            if (x < 2)
+            {
+                return false;
+            }
+            if (x == 2)
+            {
+                return true;
+            }
+            if (x % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int del = 3; del <= Math.Sqrt(x); del += 2)
+            {
+                if (x % del == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return true;
+        }
+
+        private void prepsani(TextBox xb, ListBox yb)
+        {
+            yb.Items.Clear();
+            foreach (string cisla in xb.Lines)
+            {
+                if (Prvocislo(Convert.ToInt32(cisla)))
+                {
+                    yb.Items.Add(cisla);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Random rn = new Random();
+            int n = Convert.ToInt32(textBox2.Text);
+
+            for (int i = 0; i < n; i++)
+            {
+                textBox1.Text += rn.Next(2, 16).ToString();
+                if (n - i != 1)
+                {
+                    textBox1.Text += Environment.NewLine;
+                }
+                prepsani(textBox1, listBox1);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("METODA2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("METODA2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("fd7d2811-c2f9-4e9f-8db2-c4b9279c8368")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
